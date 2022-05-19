f3aa76ea27182ffc9c39db4810be118d709f881a
Anders Aaen Springborg <aaspringborg@gmail.com>
2022-05-19 22:14:40 +0200

    Add http client to paypal service

--- src/Infrastructure/PaymentService/PayPalService.cs
+++ src/Infrastructure/PaymentService/PayPalService.cs
@@ -1,14 +1,21 @@
 using MorgenBooster.Domain.Entities;
 using MorgenBooster.Domain.Interfaces.Infrastructure;
+using Microsoft.Extensions.Logging;
 
 namespace MorgenBooster.Infrastructure.PaymentService;
 
 public class PayPalService : IPaymentService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public PayPalService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
 
     public Task<Order> Process(Order order)
     {
-        // Do api magic.
+        // Do api magic. using http client
         order.Status = OrderStatus.Paid;
         return Task.FromResult(order);
     }
--- src/IntegrationTest/PayPalTest.cs
+++ src/IntegrationTest/PayPalTest.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
 using MorgenBooster.Domain.Entities;
 using MorgenBooster.Infrastructure.PaymentService;
 using Xunit;
@@ -9,10 +11,10 @@ namespace MorgenBooster.IntegrationTest;
 public class PayPalTest
 {
     [Fact]
-    public async Task GenerateLabelForOrder()
+    public async Task PayOrder()
     {
         ServiceCollection serviceCollection = new ServiceCollection();
-
+        serviceCollection.AddHttpClient();
         serviceCollection.AddSingleton<PayPalService>();
 
         ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
